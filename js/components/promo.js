529e9badab6f633c67ca59b6a01652c360473aad
SlavaMilin <slava.milin@gmail.com>
2018-07-31 09:39:11 +0300

    add promo component

---
+++ js/components/promo.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Promo = () => {
+  return (
+    <div className="promo">
+      <h1 className="promo__title visually-hidden">Keksobooking. Кексы по соседству</h1>
+      <img src="img/keksobooking.svg" alt="Keksobooking. Кексы по соседству" width="215" height="45" />
+    </div>
+  );
+}
+
+export default Promo;
